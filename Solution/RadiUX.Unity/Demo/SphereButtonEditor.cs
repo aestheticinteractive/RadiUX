647f996975c8f0df19bab932b755f7c3cc7a93e5
Zach Kinstner <zach@aestheticinteractive.com>
2014-12-03 17:22:20 -0500

    Added a custom Unity editor for the button element.

---
+++ Solution/RadiUX.Unity/Demo/SphereButtonEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+namespace RadiUX.Unity.Demo {
+
+	/*================================================================================================*/
+	[CustomEditor(typeof(SphereButton))]
+	public class SphereButtonEditor : SphereSegmentEditor {
+
+		private SphereButton vButton;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public override void OnEnable() {
+			base.OnEnable();
+			vButton = (SphereButton)target;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override void OnInspectorGUI() {
+			base.OnInspectorGUI();
+		}
+
+	}
+
+}
